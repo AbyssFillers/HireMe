f204e92df592cd70c209088cc9b422eddb0022eb
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-02-01 14:28:12 +0530

    User table revamped

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,15 +1,29 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 type User struct {
-	ID             string        `json:"id" gorm:"primaryKey"`
-	Name           string        `json:"name"`
-	Email          string        `json:"email" gorm:"unique;not null"`
-	HashedPassword string        `json:"-"`
-	IsPremium      bool          `json:"isPremium" gorm:"default:false"`
-	Phone          string        `json:"phone"`
-	CreatedAt      time.Time     `json:"created_at"`
-	UpdatedAt      time.Time     `json:"updated_at"`
-	Applications   []Application `json:"applications" gorm:"foreignKey:UserID"`
+	ID             string    `json:"id"`
+	Name           string    `json:"name"`
+	Email          string    `json:"email"`
+	Phone          string    `json:"phone"`
+	HashedPassword string    `json:"hashed_password"`
+	IsPremium      bool      `json:"isPremium"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+
+	Applications []Application `json:"applications"`
+	Profile      UserProfile   `json:"profile"`
+}
+
+type UserProfile struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	UserID    string         `json:"user_id" gorm:"unique;not null"`
+	Bio       string         `json:"bio"`
+	ResumeURL string         `json:"resume_url"`
+	Skills    pq.StringArray `json:"skills" gorm:"type:text[]"`
 }
